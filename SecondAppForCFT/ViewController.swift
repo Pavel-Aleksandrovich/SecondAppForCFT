9ef131b7c08a2949fd239f4a1ba7da6fcd4d11c1
Pavel Mishanin <pavelmishanin@mail.ru>
2021-09-30 13:46:05 +0700

    Initial Commit

---
+++ SecondAppForCFT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondAppForCFT
+//
+//  Created by pavel mishanin on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
